4067f66854dacc950bc02c56f761de23ee5d1941
pranavbhuchahda <pranavbhuchhada@gmail.com>
2020-01-03 16:14:26 +0530

    Card class created

--- App.js
+++ App.js
@@ -1,36 +1,18 @@
 import React from 'react';
 import { StyleSheet,
-   Text,
-   View,
-   TouchableHighlight,
-   Dimensions,
-   ScrollView} from 'react-native';
-import { FontAwesome } from '@expo/vector-icons';
+  Text,
+  View,
+  TouchableHighlight,
+  Dimensions,
+  ScrollView,
+  Animated,
+  ToastAndroid} from 'react-native';
+import {responsiveFontSize,
+  responsiveWidth} from 'react-native-responsive-dimensions';
 
-class Card extends React.Component {
-  constructor(props){
-    super(props);
-    const screenWidth = Math.round(Dimensions.get('window').width);
-    this.state = {Width:screenWidth*0.4,Height:screenWidth*0.6,Margin:screenWidth*0.05}
-  }
-  onPress = ()=>{
-    return;
-  }
-  render() {
-    return (
-      <View style={{height:this.state.Height,width:this.state.Width,margin:this.state.Margin}}>
-        <TouchableHighlight style={styles.card} onPress={this.onPress} activeOpacity = {0.5} underlayColor = {"#F9F9F9"}>
-          <View style={{flex:1,width:"100%"}}>
-            <View style={styles.cImg} >
-              <Text style={{fontSize:25}} >Space For Image</Text>
-            </View>
-            <Text style={styles.card_text}>{this.props.name}</Text>
-          </View>
-        </TouchableHighlight>
-      </View>
-    );
-  }
-}
+// coustom Imports
+import Card from './components/Card'; // Card Class import
+
 export default function App() {
   return (
     <View style={styles.container}>
@@ -38,30 +20,6 @@ export default function App() {
         <Text style={styles.HeaderText}>Crypto-Calc</Text>
       </View>
       <ScrollView style={styles.sroller}>
-        <View style={styles.cRow}>
-          <Card name="Pranav Bhuchhada"/>
-          <Card name="Stavan Adhyaru"/>
-        </View>
-        <View style={styles.cRow}>
-          <Card name="Pranav Bhuchhada"/>
-          <Card name="Stavan Adhyaru"/>
-        </View>
-        <View style={styles.cRow}>
-          <Card name="Pranav Bhuchhada"/>
-          <Card name="Stavan Adhyaru"/>
-        </View>
-        <View style={styles.cRow}>
-          <Card name="Pranav Bhuchhada"/>
-          <Card name="Stavan Adhyaru"/>
-        </View>
-        <View style={styles.cRow}>
-          <Card name="Pranav Bhuchhada"/>
-          <Card name="Stavan Adhyaru"/>
-        </View>
-        <View style={styles.cRow}>
-          <Card name="Pranav Bhuchhada"/>
-          <Card name="Stavan Adhyaru"/>
-        </View>
         <View style={styles.cRow}>
           <Card name="Pranav Bhuchhada"/>
           <Card name="Stavan Adhyaru"/>
@@ -80,8 +38,6 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection:"column",
     backgroundColor: '#5bc3eb',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
   Header:{
     flex:0.1,
@@ -89,52 +45,19 @@ const styles = StyleSheet.create({
     justifyContent:"center",
     width:"100%",
     backgroundColor:"#36382e",
-    elevation:100,
   },
   HeaderText:{
-    fontSize:40,
+    fontSize:responsiveFontSize(4),
     textAlignVertical:"center",
     color:"#ede6e3",
-    textAlign:"left",
-    marginLeft:"10%"
+    marginLeft:"5%"
   },
   sroller:{
     flex:0.85,
     width:"100%"
   },
-  card: {
-    flex:1,
-    alignItems:"center",
-    justifyContent:"center",
-    backgroundColor:"white",
-    borderWidth:0.2,
-    borderColor:"#AAA",
-    borderRadius:2,
-    shadowColor: 'red',
-    shadowOffset: {width: 1,height:1 },
-    shadowOpacity: 1,
-    shadowRadius: 1,
-    elevation: 10,
-  },
-  card_text:{
-    flex:1,
-    width:"100%",
-    fontSize:30,
-    textTransform:"uppercase",
-    fontFamily:"monospace",
-    textShadowColor:"black",
-    backgroundColor:"#36382e",
-    textAlign:"center",
-    textAlignVertical:"center",
-    color:"#dadad9"
-  },
+
   cRow:{
     flexDirection:"row",
-  },
-  cImg:{
-    flex:2,
-    backgroundColor:"#f06449",
-    justifyContent:"center",
-    alignItems:"center"
   },
 });
--- components/Card.js
+++ components/Card.js
@@ -1,25 +1,58 @@
 import React from 'react';
-import StyleSheet from 'react-native';
-class Card {
+import { StyleSheet,
+  Text,
+  View,
+  TouchableHighlight,
+  Animated,} from 'react-native';
+import {responsiveFontSize,
+  responsiveWidth} from 'react-native-responsive-dimensions';
+
+export default class Card extends React.Component {
+  constructor(props){
+    super(props);
+    this.state ={
+    }
+    this.cardStyle = StyleSheet.create({
+      card: {
+        flex:1,
+        backgroundColor:"white",
+        elevation: 10,
+        opacity:1,
+      },
+      card_text:{
+        flex:1,
+        width:"100%",
+        fontSize: responsiveFontSize(3),
+        textTransform:"uppercase",
+        // fontFamily:"monospace",
+        textShadowColor:"black",
+        backgroundColor:"#36382e",
+        textAlign:"center",
+        textAlignVertical:"center",
+        color:"#dadad9"
+      },
+      cImg:{
+        flex:2,
+        backgroundColor:"#f06449",
+        justifyContent:"center",
+        alignItems:"center"
+      },
+    });
+  }
+  onPress = ()=>{
+  }
   render() {
     return (
-      <View style={cardStyle.card}>
-        <TouchableHighlight onPress={this.color='red'} underlayColor={"#f1f1f1"}>
-        </TouchableHighlight>   
+      <View style={{height:responsiveWidth(60),width:responsiveWidth(40),margin:responsiveWidth(5)}}>
+        <TouchableHighlight style = {this.cardStyle.card}  onPressedIn={this.onPressedIn} onPressOut={this.onPressedOut} onPress={this.onPress} activeOpacity = {0.5} underlayColor = {"#F9F9F9"}>
+          <Animated.View style={{flex:1,width:"100%"}}>
+            <View style={this.cardStyle.cImg} >
+              <Text style={{fontSize:25,textAlign:"center"}} >Space For Image</Text>
+            </View>
+            <Text style={this.cardStyle.card_text}>{this.props.name}</Text>
+          </Animated.View>
+        </TouchableHighlight>
       </View>
     );
   }
 }
-const cardStyle = StyleSheet.create({
-  card: {
-    // flex: 1,
-    width:"50%",
-    alignItems: 'center',
-    borderColor:'black',
-    borderWidth:3
-  },
-  card_text: {
-    fontSize: 50,
-    fontWeight: 'bold'
-  }
-});
